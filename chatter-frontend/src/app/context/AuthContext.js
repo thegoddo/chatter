84f51877810a61fd1cf64d767378ff8c7575d877
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-10-29 11:49:03 +0530

    create auth context

---
+++ chatter-frontend/src/app/context/AuthContext.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }
+
+  const login = async (username, password) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/auth/login",
+        {
+          username,
+          password,
+        }
+      );
+      const jwtToken = response.data;
+      setToken(jwtToken);
+      setUser({ username });
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      localStorage.setItem("token", jwtToken);
+      return true;
+    } catch (error) {
+      console.error("Login failed: ", error);
+      return false;
+    }
+  };
+
+  const register = async (username, password, phoneNumber) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/auth/register",
+        {
+          username,
+          password,
+          phoneNumber,
+          status: "OFFLINE",
+        }
+      );
+
+      return true;
+    } catch (error) {
+      console.error("Registration failed: ", error);
+      return false;
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem("token");
+    delete axios.defaults.headers.common["Authorization"];
+  };
+
+  const value = [user, token, login, register, logout];
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
