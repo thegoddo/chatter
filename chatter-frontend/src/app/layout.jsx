4035a2156c5435f2b5c4a042c7391c6419716a0c
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-10-29 12:04:56 +0530

    change root element

---
+++ chatter-frontend/src/app/layout.jsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+import { AuthProvider } from "./context/AuthContext";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
+}
